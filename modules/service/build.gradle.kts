199080bc9c4582ff1f115e5bcc2f6f47b45910ea
Alexander V. Zinin <mail@zinin.ru>
2026-03-16 10:11:57 +0300

    build(service): migrate to Version Catalog

--- modules/service/build.gradle.kts
+++ modules/service/build.gradle.kts
@@ -11,23 +11,19 @@ dependencies {
     implementation(project(":frigate-analyzer-common"))
     api(project(":frigate-analyzer-model"))
 
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.data:spring-data-r2dbc")
+    implementation(libs.spring.boot.starter)
+    implementation(libs.spring.data.r2dbc)
+    implementation(libs.uuid.creator)
+    implementation(libs.kotlin.logging)
+    implementation(libs.bundles.mapstruct)
+    implementation(libs.jakarta.inject.api)
 
-    implementation("com.github.f4b6a3:uuid-creator:${property("uuidCreatorVersion")}")
+    kapt(libs.mapstruct.processor)
 
-    implementation("io.github.oshai:kotlin-logging-jvm:${property("kotlinLoggingVersion")}")
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.kotlin.test.junit5)
 
-    implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
-    implementation("org.mapstruct.extensions.spring:mapstruct-spring-extensions:${property("mapstructSpringExtensionsVersion")}")
-    implementation("jakarta.inject:jakarta.inject-api:${property("jakartaInjectApiVersion")}")
-
-    kapt("org.mapstruct:mapstruct-processor:${property("mapstructVersion")}")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 kapt {
