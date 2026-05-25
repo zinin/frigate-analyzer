4179ac2dda723068e3600c7365864ea3845c9845
Alexander V. Zinin <mail@zinin.ru>
2026-05-25 17:55:50 +0300

    refactor: remove /statistics endpoint superseded by /status

--- modules/model/src/main/kotlin/ru/zinin/frigate/analyzer/model/response/StatisticsResponse.kt
+++
@@ -1,6 +0,0 @@
-package ru.zinin.frigate.analyzer.model.response
-
-data class StatisticsResponse(
-    val recordings: RecordingsStatistics,
-    val detectServers: List<DetectServerStatistics>,
-)
