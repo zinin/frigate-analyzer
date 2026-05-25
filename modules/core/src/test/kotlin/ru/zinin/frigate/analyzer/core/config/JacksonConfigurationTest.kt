3283a9ada64da0ab3a81faca60b0b7ce20e10e30
Alexander V. Zinin <mail@zinin.ru>
2026-05-25 17:48:57 +0300

    style(test): remove unused readValue import in JacksonConfigurationTest

--- modules/core/src/test/kotlin/ru/zinin/frigate/analyzer/core/config/JacksonConfigurationTest.kt
+++ modules/core/src/test/kotlin/ru/zinin/frigate/analyzer/core/config/JacksonConfigurationTest.kt
@@ -1,6 +1,5 @@
 package ru.zinin.frigate.analyzer.core.config
 
-import com.fasterxml.jackson.module.kotlin.readValue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.Duration
